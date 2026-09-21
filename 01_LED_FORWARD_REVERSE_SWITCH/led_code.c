1666c91b3bb7163e131508a77c62848507027888
abilashkl24 <abilashkl24puthoor@gmail.com>
2026-09-21 22:43:45 +0530

    updates

--- 01_LED_FORWARD_REVERSE_SWITCH/led_code.c
+++ 01_LED_FORWARD_REVERSE_SWITCH/led_code.c
@@ -1,1 +1,66 @@
-// byrhbybhthybtgb6ujbn
+#include <pic.h>
+
+#define led1 RB0
+#define led2 RB1
+#define led3 RB2
+#define led4 RB3
+#define led5 RB4
+#define led6 RB5
+#define led7 RB6
+#define led8 RB7
+
+#define sw RD0
+
+void delay();
+
+void main()
+{
+    unsigned int led;
+
+    TRISB = 0x00;      // PORTB as output
+    PORTB = 0x00;      // All LEDs OFF
+
+    TRISD0 = 1;        // RD0 as input
+    
+    while(1)
+    {
+        if(sw == 0)
+        {
+            delay();         
+
+            if(sw == 0)
+            {
+                for(led = 0; led < 8; led++)
+                {
+                    if(sw != 0)       // switch released
+                    {
+                        PORTB = 0x00;  // LEDs OFF
+                        break;  //sequence off
+                    }
+
+                    PORTB = (1 << led);
+                    delay();
+                }
+                for(led = 6; led > 0; led--)
+                {
+                    if(sw != 0)
+                    {
+                        PORTB = 0x00;   //LEDs OFF
+                        break;          // Sequence  off
+                    }
+                    PORTB = (1 << led);
+                    delay();
+                }
+
+                PORTB = 0x00;
+            }
+        }
+    }
+}
+
+void delay()
+{
+    unsigned int i;
+
+    for(i = 0; i < 1000; i++);
+}
